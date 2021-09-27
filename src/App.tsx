92432651852e72d910a44fa98ee74ad074c6b517
Jacco Klouwer <Jacco.Klouwer@conclusion.nl>
2021-09-27 11:25:50 +0200

    first changes

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Dit is een test.
+          Welcome to codaisseur!
         </a>
       </header>
     </div>
